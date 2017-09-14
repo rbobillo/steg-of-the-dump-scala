8e7abe50149f24fc97b8ce2f153541ee29fa4461
Raphael Bobillot <raphael.bobillot-prestataire@laposte.fr>
2017-09-14 17:53:17 +0200

    preparing decoding

--- Decoder.scala
+++ Decoder.scala
@@ -1,5 +1,5 @@
 case class Decoder(homoglyphsDict: Map[Char, Array[Char]], secretAlphabet: String) extends Utils {
 
-  def decode(message: String) = ???
+  def decode(message: String) = message
 
 }
